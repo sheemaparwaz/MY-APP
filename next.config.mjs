8e5c00ab07365a1f40a41157172c0c2cd42b3044
sheemaparwaz <sheemaparwaz@gmail.com>
2024-02-29 15:20:04 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
+/**original config */
+/**
+ //  @type {import('next').NextConfig} 
 const nextConfig = {
   reactStrictMode: true,
 };
-
 export default nextConfig;
+ */
+
+
+const isProd = process.env.NODE_ENV === 'production';
+
+//export const assetPrefix = isProd ? '/MY-APP' : '';
+//export const basePath = isProd ? '/MY-APP' : '';
+//const isProd = process.env.NODE_ENV === 'production';
+
+export const output = 'export';
+export const basePath = isProd ? '/MY-APP' : '';
+export const assetPrefix = isProd ? '/MY-APP' : '';
